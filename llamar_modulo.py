0cc337fdbeb70fc650063bc9f403bfe4706208d6
iortizdeluzu <123472816+iortizdeluzu@users.noreply.github.com>
2025-12-30 15:03:35 +0100

    Add files via upload

---
+++ llamar_modulo.py
@@ -0,0 +1,7 @@
+# Este fichero llama al modulo siempre que este en mismo directorio 
+# Tambien si la funcion esta dentro de la variable PYTHONPATH
+
+import solicitar_datos
+
+
+solicitar_datos.datos()
---
+++ setup.py
@@ -0,0 +1,14 @@
+# Fichero instalador
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mimodulo",           # Nombre del paquete
+    version="0.1",              # Versión inicial
+    packages=find_packages(),   # Encuentra automáticamente las carpetas con __init__.py
+    install_requires=[],        # Dependencias externas si las hay
+    author="Iker Ortiz de Luzuriaga",
+    author_email="iker.ortiz@dipc.org",
+    description="Un módulo de ejemplo que solicita datos",
+    url="https://github.com/tuusuario/mimodulo",
+)
