93f47293a6f27b799f1554fdec08e44128c8abfe
Iker Ortiz de Luzuriaga <iker.ortiz@dipc.org>
2025-12-30 16:28:14 +0100

    added to PyPi repo

--- setup.py
+++ setup.py
@@ -15,4 +15,10 @@ setup(
     "pandas",
     "requests"
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
